7fcab4a8f6dfc483e35105b849f27e23ad2738e0
Jonathan Wendt <wendt@vr.rwth-aachen.de>
2020-01-17 17:12:00 +0100

    remove public path warning

--- Source/UniversalLogging/UniversalLogging.Build.cs
+++ Source/UniversalLogging/UniversalLogging.Build.cs
@@ -6,7 +6,7 @@ public class UniversalLogging : ModuleRules
     public UniversalLogging(ReadOnlyTargetRules Target) : base(Target)
     {
         PrivateIncludePaths.AddRange(new string[] { "UniversalLogging/Private" });
-        PublicIncludePaths.AddRange(new string[] { "UniversalLogging/Public" });
+        PublicIncludePaths.AddRange(new string[] {  });
 
         PublicDependencyModuleNames.AddRange(new string[] { "CoreUObject", "Engine", "Core", "DisplayCluster" });
     }
